d71150ead10f804d430bf549912d3a288ed8e0fc
sujin lee <jmakeaz@gmail.com>
2022-01-12 17:50:04 +0900

    bcrypt를 이용해 비밀번호 암호화

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
-   name: {
+    name: {
        type: String,
        maxLength: 50
    },
@@ -28,6 +30,25 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
        type: Number
+    }
+});
+
+// DB에 save 하기 전에 pre 실행
+// next() 함수로 save 실행
+userSchema.pre("save", function (next) {
+    const user = this;
+    console.log(user);
+    // 비밀번호를 변경할 때만
+    // 비밀번호를 암호화 시킨다.
+    if (user.isModified("password")) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
     }
 });
 
