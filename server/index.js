feb977981db5b036005ff59a550dadc87a2982d8
sujin lee <jmakeaz@gmail.com>
2022-01-19 18:31:35 +0900

    Proxy설정으로 CORS 해결

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const bodyParser = require("body-parser")
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { auth } = require("./middleware/auth");
 const { User } = require("./models/user");
@@ -9,25 +9,29 @@ const { User } = require("./models/user");
 const config = require("./config/key");
 
 // application/x-www-form-urlencoded 코드로 된 것을 가져올 수 있게 설정
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // application/json 코드로 된 것을 가져올 수 있게 설정
 app.use(bodyParser.json());
 
 app.use(cookieParser());
 
-const mongoose = require("mongoose")
-mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+const mongoose = require("mongoose");
+mongoose
+    .connect(config.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => console.log("MongoDB Connected.."))
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 
+app.get("/", (req, res) => {
+    res.send("Hello World! nodemon!");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World! nodemon!')
-})
+app.get("/api/hello", (req, res) => {
+    res.send("hello~ welcome");
+});
 
 app.post("/api/user/register", (req, res) => {
     // 회원가입에 필요한 정보를 client에서 가져오면
@@ -52,12 +56,12 @@ app.post("/api/user/login", (req, res) => {
             return res.json({
                 loginSuccess: false,
                 message: "제공된 이메일에 해당하는 유저가 없습니다."
-            })
+            });
         }
 
         // 비밀번호 확인
         user.comparePassword(req.body.password, (err, isMatch) => {
-            if(!isMatch) {
+            if (!isMatch) {
                 return res.json({
                     loginSuccess: false,
                     message: "비밀번호가 틀렸습니다."
@@ -69,15 +73,13 @@ app.post("/api/user/login", (req, res) => {
                 if (err) return res.status(400).send(err);
 
                 // 토큰을 저장한다. 쿠키, 세션, 로컬스토리지
-                res.cookie("x_auth", user.token)
-                    .status(200)
-                    .json({
-                        loginSuccess: true,
-                        userId: user._id
-                    });
+                res.cookie("x_auth", user.token).status(200).json({
+                    loginSuccess: true,
+                    userId: user._id
+                });
             });
         });
-    })
+    });
 });
 
 app.get("/api/user/auth", auth, (req, res) => {
@@ -86,7 +88,8 @@ app.get("/api/user/auth", auth, (req, res) => {
         isAdmin: req.user.role !== 0,
         isAuth: true,
         email: req.user.email,
-        name: req.user,name,
+        name: req.user,
+        name,
         lastname: req.user.lastname,
         role: req.user.role,
         image: req.user.image
@@ -95,20 +98,18 @@ app.get("/api/user/auth", auth, (req, res) => {
 
 app.get("/api/user/logout", auth, (req, res) => {
     // 유저의 아이디로 찾아 토큰을 지운다
-    User.findOneAndUpdate(
-        { _id: req.user._id },
-        {token: ""},
-        (err, user) => {
-            if (err) return res.json({
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+        if (err)
+            return res.json({
                 success: false,
                 err
-            })
-            return res.status(200).send({
-                success: true
             });
-        })
+        return res.status(200).send({
+            success: true
+        });
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
