4a2728e06d9d2c93d487c67934563e0ef2ec08cb
sujin lee <jmakeaz@gmail.com>
2022-01-20 14:59:47 +0900

    redux 구성

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import "antd/dist/antd.min.css";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+    promiseMiddleware,
+    ReduxThunk
+)(createStore);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider
+            store={createStoreWithMiddleware(
+                Reducer,
+                window.__REDUX_DEVTOOLS_EXTENSION__ &&
+                    window.__REDUX_DEVTOOLS_EXTENSION__()
+            )}
+        >
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
